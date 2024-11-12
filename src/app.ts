365f6f32734f8d098960985b8e3fe6658a0bae55
Megagig <megagigsoftwaresolutions.com>
2024-11-12 14:49:14 +0100

    Implicitly check the types

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-const app = express();
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
