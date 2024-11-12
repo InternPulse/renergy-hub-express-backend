3eeda3eec0ab36d54ce55063a80ed586f21a7227
Megagig <megagigsoftwaresolutions.com>
2024-11-12 16:14:12 +0100

    Environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from 'express';
+import { PORT } from './secrets.ts';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
