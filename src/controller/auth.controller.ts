7e48b1f0d52c1bdd7e24af4371df3be601b5dd2a
Megagig <megagigsoftwaresolutions.com>
2024-11-14 20:19:43 +0100

    Validation and hash password

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,39 @@
+import bcryptjs from 'bcryptjs';
 import { Request, Response } from 'express';
+import prisma from '../../db/prisma';
 
-export const register = async (req: Request, res: Response) => {};
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, username, email, password, confirmPassword } =
+      req.body;
+
+    //validate user input
+    if (
+      !firstName ||
+      !lastName ||
+      !username ||
+      !email ||
+      !password ||
+      !confirmPassword
+    ) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+    // check if password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    const user = await prisma.user.findUnique({ where: { email, username } });
+    // check if user already exists
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // hash password before saving to database
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+  } catch (error) {}
+};
 export const login = async (req: Request, res: Response) => {};
 export const logout = async (req: Request, res: Response) => {};
 export const forgotPassword = async (req: Request, res: Response) => {};
