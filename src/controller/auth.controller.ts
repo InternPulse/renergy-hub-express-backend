194226131daf768d1b41ff72668d17f83e1f6ebb
Megagig <megagigsoftwaresolutions.com>
2024-11-14 23:30:00 +0100

    Implement the Loggout funfuctionality

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,6 +2,7 @@ import bcryptjs from 'bcryptjs';
 import { Request, Response } from 'express';
 import prisma from '../../db/prisma';
 import generateToken from '../util/generateToken';
+import { Prisma } from '@prisma/client';
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -69,12 +70,63 @@ export const register = async (req: Request, res: Response) => {
       res.status(400).json({ message: 'Invalid user data: User not created' });
     }
   } catch (error: any) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        // Unique constraint failed
+        return res.status(400).json({ message: 'Username already exists' });
+      }
+    }
     console.log('Error in the Register controller', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
-export const login = async (req: Request, res: Response) => {};
-export const logout = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    // check if email and password are provided
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Email and password are required' });
+    }
+    //check if the user exists in the database
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // check if password is correct
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // generate token
+    generateToken(user.id.toString(), res);
+
+    // return user data
+    res.status(200).json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+    });
+  } catch (error: any) {
+    console.log('Error in the Login controller', error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+export const logout = async (req: Request, res: Response) => {
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error: any) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 export const forgotPassword = async (req: Request, res: Response) => {};
 export const resetPassword = async (req: Request, res: Response) => {};
 export const verifyEmail = async (req: Request, res: Response) => {};
