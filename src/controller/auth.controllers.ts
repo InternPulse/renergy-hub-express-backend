d9678fc900944f8daab9ef058b4b2f8fb52685dd
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-11-16 21:10:41 +0100

    added validateUserDetails functionality

--- src/controller/auth.controllers.ts
+++ src/controller/auth.controllers.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import prisma from "../util/db";
+import { validateUserDetails } from "../util/validateUserDetails";
 
 export const registerUser = async (req: Request, res: Response) => {
-  res.send("User registered");
+  try {
+    let result = validateUserDetails(req.body);
+
+    if (result == "rejected") {
+      res
+        .status(400)
+        .json({
+          status: "error",
+          code: "400",
+          message: "All fields are required",
+        });
+    }
+
+    res.status(200).json(req.body);
+  } catch (error: any) {
+    console.log(error.message);
+  }
 };
---
+++ src/util/validateUserDetails.ts
@@ -0,0 +1,28 @@
+export const validateUserDetails = (userDetails: any) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      confirmPassword,
+      phoneNumber,
+    } = userDetails;
+
+    if (
+      !firstName ||
+      !lastName ||
+      !username ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !phoneNumber
+    ) {
+      return "rejected";
+    }
+    return "accepted";
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
