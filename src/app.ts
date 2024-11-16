d8fd41e420d5130d9a7529c27032ecba137273a0
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-11-16 18:47:05 +0100

    initialized user register route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
-import  { Express, Request, Response } from 'express';
-import { Route } from './routes/route';
-import express from 'express';
-import { OrderRoute } from './routes/orderRoute';
-import { PORT } from './secrets';
-import { initRoutes } from './routes/init';
-import { errorHandler } from './util';
+import { Express, Request, Response } from "express";
+import { Route } from "./routes/route";
+import express from "express";
+import { OrderRoute } from "./routes/orderRoute";
+import { PORT } from "./secrets";
+import { initRoutes } from "./routes/init";
+import { errorHandler } from "./util";
+import authRoutes from "./routes/auth.routes";
 
 const app: Express = express();
 // Apply middleware
 app.use(express.json());
 
-
+//auth routes
+app.use("/api/v1/auth", authRoutes);
 
 initRoutes(app);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
@@ -23,6 +25,4 @@ app.listen(PORT, () => {
 });
 
 //GLobal error Handler
-app.use(errorHandler)
-
-
+app.use(errorHandler);
