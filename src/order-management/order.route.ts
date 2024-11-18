5a157b30eca438f9b41339985d5776e68a2e18aa
el-collins <charlescollins476@gmail.com>
2024-11-18 11:22:45 +0100

    ✨ feat: Add WishList management APIs
    - Implemented API to create a new WishList
    - Implemented API to get a WishList by ID
    - Implemented API to get all WishLists for a User
    - Implemented API to update a WishList by ID
    - Implemented API to delete a WishList by ID
    - Added routes for WishList management in order.route.ts
    - Created wishlist.controller.ts for handling WishList requests
    - Created wishlist.service.ts for WishList business logic : 2024-11-18 10:22:45 ✨

--- src/order-management/order.route.ts
+++ src/order-management/order.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { createOrder, getAllOrders } from "./order.controller";
 import { Route } from "../util/route";
 import { deleteOrder, getOrderById, updateOrder } from "./order.service";
+import { createWishList, getWishListById, getAllWishListsForUser, updateWishList, deleteWishList } from "./wishlist.controller";
+
 
 export class OrderRoute extends Route {
 	readonly name: string = 'orders';
@@ -16,6 +18,14 @@ export class OrderRoute extends Route {
 		.get('/:orderId', getOrderById)
 		.put('/:orderId', updateOrder)
 		.delete('/:orderId', deleteOrder);
+
+		  // WishList routes
+		  this.router
+		  .post('/wishlist', createWishList)
+		  .get('/wishlist/:wishlistId', getWishListById)
+		  .get('/user/:userId/wishlist', getAllWishListsForUser)
+		  .put('/wishlist/:wishlistId', updateWishList)
+		  .delete('/wishlist/:wishlistId', deleteWishList);
 
 		return this.router;
 	}
