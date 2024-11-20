55eb56f498f9c9ac5f1d585b13c10c75fc119403
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-11-20 06:43:52 +0100

    added isVerified field to schema

--- prisma/migrations/20241116191110_init2/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "confirmPassword" TEXT,
-ADD COLUMN     "resetToken" TEXT,
-ADD COLUMN     "resetTokenExpiresAt" TIMESTAMP(3),
-ADD COLUMN     "verificationToken" TEXT,
-ADD COLUMN     "verificationTokenExpiresAt" TIMESTAMP(3);
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,3 +1,13 @@
 require("dotenv").config({ path: "./.env" });
 
 export const PORT = process.env.PORT;
+
+export const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+export const NODE_ENV = process.env.NODE_ENV;
