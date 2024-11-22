cdf270d46aae53fdf5fc21b7738d8b1510127162
wolecodes <opeyemioyeyemi141@gmail.com>
2024-11-22 16:10:29 +0100

    fix : dependencies issues with express to allow server to run

--- src/order-management/order.route.ts
+++ src/order-management/order.route.ts
@@ -1,38 +1,45 @@
 import { Router } from "express";
-import { createOrder, createorderitemhandler, getAllOrders, getOrderItemById, updateOrderItemhandler } from "./order.controller";
+import {
+  createOrder,
+  createorderitemhandler,
+  getAllOrders,
+  getOrderItemById,
+  updateOrderItemhandler,
+} from "./order.controller";
 import { Route } from "../util/route";
 import { deleteOrder, getOrderById, updateOrder } from "./order.service";
-import { createWishList, getWishListById, getAllWishListsForUser, updateWishList, deleteWishList } from "./wishlist.controller";
-
+import {
+  createWishList,
+  getWishListById,
+  getAllWishListsForUser,
+  updateWishList,
+  deleteWishList,
+} from "./wishlist.controller";
 
 export class OrderRoute extends Route {
-	readonly name: string = 'orders';
+  readonly name: string = "orders";
 
+  initRoutes(): Router {
+    this.router.post("/", createOrder).get("/", getAllOrders);
 
-	initRoutes(): Router {
-		this.router
-		.post('/', createOrder)
-		.get('/', getAllOrders);
+    this.router
+      .get("/:orderId", getOrderById)
+      .put("/:orderId", updateOrder)
+      .delete("/:orderId", deleteOrder);
 
-		this.router
-		.get('/:orderId', getOrderById)
-		.put('/:orderId', updateOrder)
-		.delete('/:orderId', deleteOrder);
+    // WishList routes
+    this.router
+      .post("/wishlist", createWishList)
+      .get("/wishlist/:wishlistId", getWishListById)
+      .get("/user/:userId/wishlist", getAllWishListsForUser)
+      .put("/wishlist/:wishlistId", updateWishList)
+      .delete("/wishlist/:wishlistId", deleteWishList);
 
-		  // WishList routes
-		  this.router
-			.post('/wishlist', createWishList)
-			.get('/wishlist/:wishlistId', getWishListById)
-			.get('/user/:userId/wishlist', getAllWishListsForUser)
-			.put('/wishlist/:wishlistId', updateWishList)
-			.delete('/wishlist/:wishlistId', deleteWishList);
-			
-		  this.router
-			.post('/createorderitem',createorderitemhandler)
-			.get('/:id',getOrderItemById)
-			.put('/:id',updateOrderItemhandler)
+    this.router
+      .post("/createorderitem", createorderitemhandler)
+      .get("/:id", getOrderItemById)
+      .put("/:id", updateOrderItemhandler);
 
-
-		return this.router;
-	}
+    return this.router;
+  }
 }
