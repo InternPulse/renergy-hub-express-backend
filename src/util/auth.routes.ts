df702c678dcd15e3af03084388e586871782cd4a
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-11-23 21:01:02 +0100

    added forgrt password, reset password and other functionalities

--- src/util/auth.routes.ts
+++ src/util/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import passport from './passportConfig';
+import passport from "./passportConfig";
 import {
   registerUser,
   verifyEmail,
@@ -7,7 +7,12 @@ import {
   login,
   logout,
   googleCallback,
-  facebookCallback
+  facebookCallback,
+  requestPasswordReset,
+  verifyPasswordReset,
+  resetPassword,
+  forgotPassword,
+  verifyForgotPassword,
 } from "../user-management/controller/auth.controllers";
 
 const router = Router();
@@ -17,14 +22,28 @@ router.post("/verify", verifyEmail);
 router.get("/resend/:id", resendEmail);
 router.post("/login", login);
 router.get("/logout", logout);
-router.get('/google', passport.authenticate('google', { scope: ['profile', "email"] }));
-router.get('/google/callback', passport.authenticate('google', {session: false}), googleCallback);
-router.get('/facebook', passport.authenticate('facebook', { scope: ["email"] }));
-router.get('/facebook/callback', passport.authenticate('facebook', {session: false}), facebookCallback);
-
-
-
-
-
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  googleCallback
+);
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { session: false }),
+  facebookCallback
+);
+router.post("/request-password-reset", requestPasswordReset);
+router.get("/verify-password-reset", verifyPasswordReset);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-forgot-password", verifyForgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
