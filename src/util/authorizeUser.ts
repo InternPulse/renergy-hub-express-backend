5bcbfec9ff8eb50ab2c791b2dff66a34477518db
Bensonate <bensonate98@gmail.com>
2024-11-30 02:11:45 -0800

    modifiedauthentication middleware

--- src/util/authorizeUser.ts
+++ src/util/authorizeUser.ts
@@ -1,24 +1,28 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
-import { JWT_SECRET } from './secrets';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "./secrets";
 
 export const verifyUserToken = async (req: Request, res: Response, next: NextFunction)=>{
   const token = req.cookies.accessToken;
   if(!token){
     return res.status(401).json({
-      sucess: false,
-      error: {
-        message: "Unauthorized user"
-      }
+      status: "error",
+      message: "token not found",
+      description: "user not authorized to access this route",
     });
   }
   try{
     const verifiedToken = jwt.verify(token, JWT_SECRET);
-    req.user = verifiedToken;
-    console.log("User Data", req.user)
+    if(!verifiedToken){
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid or expired token",
+        description: "The stored token is invalid or has expired"
+      });
+    }
     next();
   }
-  catch(error){
-    next(error)
-  } 
+  catch(err) {
+    console.log(err);
+  }
 }
