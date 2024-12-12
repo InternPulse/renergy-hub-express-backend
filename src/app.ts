a47618fdf85f2afdcc5bc4fc9bd31c4fd06215f5
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-12-12 17:37:23 +0100

    modified cors

--- src/app.ts
+++ src/app.ts
@@ -21,11 +21,12 @@ app.use(express.json());
 
 // app.use(cors());
 
-
 app.use(function (_, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
